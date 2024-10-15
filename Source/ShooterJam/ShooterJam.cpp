c69e8804d7e15a58b71bcf19f11db20778abf39c
Ivan Smialko <ivan.smialko@ab-games.com>
2024-10-15 22:04:51 +0300

    Initial project commit

---
+++ Source/ShooterJam/ShooterJam.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShooterJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShooterJam, "ShooterJam" );
