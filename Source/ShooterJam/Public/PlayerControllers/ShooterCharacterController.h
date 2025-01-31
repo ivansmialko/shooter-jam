30bbd64d3c287f9c76ad51f3276a54ddf4d24074
ivansmialko <ivansmyalko@gmail.com>
2025-01-31 22:59:48 +0200

    [Section 9][Match States] Calculating Rount-Trip-Time, cleint-server delta and finally synchroizing servertime

--- Source/ShooterJam/Public/PlayerControllers/ShooterCharacterController.h
+++ Source/ShooterJam/Public/PlayerControllers/ShooterCharacterController.h
@@ -19,15 +19,39 @@ class SHOOTERJAM_API AShooterCharacterController : public APlayerController
 
 //private variables
 private:
+
+	/** Frequency, controller is requesting servertime with */
+	UPROPERTY(EditAnywhere, Category = Time);
+	float TimeSyncFrequency{ 5.f };
+
+	/** Reference to player's heads up display */
 	AShooterHUD* ShooterHud;
+
+	/** Difference between client and server time */
+	float ClientServerDelta{ 0.f };
+
+	/** Timer, controller is using to request servertime with TimeSyncFrequency*/
+	float TimeSyncTimer{ TimeSyncFrequency };
+
 	float MatchTime{ 120.f };
 	int32 MatchTimeLeft{ 0 };
 
 //private methods
 private:
 
+	/** Sets current match time to HUD */
+	void SetHudTime();
+
 	/** Initialized hud variables */
 	bool CheckInitHud();
+
+	/** Requests the current server time, passing in the client's time when the request was sent */
+	UFUNCTION(Server, Reliable)
+	void Server_RequestServerTime(float InTimeOfClientRequest);
+
+	/** Reports the current time to the client, in response to Server_RequestServerTime */
+	UFUNCTION(Client, Reliable)
+	void Client_ReportServerTime(float InTimeOfClientRequest, float InServerTime);
 
 //protected methods
 protected:
@@ -35,18 +59,23 @@ protected:
 	virtual void BeginPlay() override;
 	//~ End AActor Interface
 
-	void SetHudTime();
 
 //public methods
 public:
 
 	//~ Begin APlayerController Interface
-	void OnPossess(APawn* InPawn) override;
+	virtual void OnPossess(APawn* InPawn) override;
+
+	/** Sync with server clock ASAP */
+	virtual void ReceivedPlayer() override;
 	//~ End APlayerController Interface
 
 	//~ Begin AActor Interface
 	virtual void Tick(float DeltaSeconds) override;
 	//~ End AActor Interface
+
+	/** Current world time on server, synchronized */
+	virtual float GetServerTime();
 
 	void SetHudHealth(float InHealth, float InMaxHealth);
 	void SetHudScore(float InScore);
