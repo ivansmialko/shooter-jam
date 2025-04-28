582ee8d4a37e1f8b1de2611645618f8bb9fae67e
ivan.smialko <ivan.smialko@ab-games.com>
2025-04-28 21:58:47 +0300

    [Section 12][Lag compensation] Compensation component class and concepts

---
+++ Source/ShooterJam/Public/Components/LagCompensationComponent.h
@@ -0,0 +1,28 @@
+// Made by smialko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LagCompensationComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SHOOTERJAM_API ULagCompensationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	ULagCompensationComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
