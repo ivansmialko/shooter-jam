9559c1faf01eb53d61b34893c3842c648afcdcb1
ivansmialko <ivansmyalko@gmail.com>
2025-05-13 23:22:28 +0300

    [Section 12][More multiplayer features] Game menu basic code for exiting match

--- Source/ShooterJam/Private/HUD/GameMenu.cpp
+++ Source/ShooterJam/Private/HUD/GameMenu.cpp
@@ -3,3 +3,101 @@
 
 #include "HUD/GameMenu.h"
 
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/GameModeBase.h"
+#include "Components/Button.h"
+#include "MultiplayerSessionsSubsystem.h"
+
+void UGameMenu::MenuSetup()
+{
+	AddToViewport();
+	SetVisibility(ESlateVisibility::Visible);
+	SetIsFocusable(true);
+
+	UWorld* World{ GetWorld() };
+	if (!World)
+		return;
+
+	PlayerController = (!PlayerController ? World->GetFirstPlayerController() : PlayerController);
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetWidgetToFocus(TakeWidget());
+
+	PlayerController->SetInputMode(InputModeData);
+	PlayerController->SetShowMouseCursor(true);
+
+	UGameInstance* GameInstance = GetGameInstance();
+	if (!GameInstance)
+		return;
+
+	MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
+	if (!MultiplayerSessionsSubsystem)
+		return;
+
+	MultiplayerSessionsSubsystem->MultiplayerOnDestroySessionComplete.AddDynamic(this, &UGameMenu::OnDestroySession);
+}
+
+void UGameMenu::MenuTeardown()
+{
+	RemoveFromParent();
+
+	UWorld* World{ GetWorld() };
+	if (!World)
+		return;
+
+	PlayerController = (!PlayerController ? World->GetFirstPlayerController() : PlayerController);
+
+	FInputModeGameOnly InputModeData;
+	PlayerController->SetInputMode(InputModeData);
+	PlayerController->SetShowMouseCursor(false);
+}
+
+bool UGameMenu::Initialize()
+{
+	if (!Super::Initialize())
+		return false;
+
+	if (ExitButton)
+	{
+		ExitButton->OnClicked.AddDynamic(this, &UGameMenu::OnExitButtonClicked);
+	}
+
+	return true;
+}
+
+void UGameMenu::OnDestroySession(bool bWasSuccessfull)
+{
+	if (!bWasSuccessfull)
+	{
+		ExitButton->SetIsEnabled(true);
+		return;
+	}
+
+	UWorld* World{ GetWorld() };
+	if (!World)
+		return;
+
+	AGameModeBase* GameMode{ World->GetAuthGameMode<AGameModeBase>() };
+	if (GameMode)
+	{
+		GameMode->ReturnToMainMenuHost();
+	}
+	else
+	{
+		PlayerController = (!PlayerController ? World->GetFirstPlayerController() : PlayerController);
+		if (PlayerController)
+		{
+			PlayerController->ClientReturnToMainMenuWithTextReason(FText());
+		}
+	}
+}
+
+void UGameMenu::OnExitButtonClicked()
+{
+	ExitButton->SetIsEnabled(false);
+
+	if (!MultiplayerSessionsSubsystem)
+		return;
+
+	MultiplayerSessionsSubsystem->DestroySession();
+}
--- Source/ShooterJam/ShooterJam.Build.cs
+++ Source/ShooterJam/ShooterJam.Build.cs
@@ -8,7 +8,7 @@ public class ShooterJam : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara", "MultiplayerSessions", "OnlineSubsystemSteam" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
