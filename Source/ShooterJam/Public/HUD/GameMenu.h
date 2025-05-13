e551dc62d3fc5b753c84359f0524143b9a4486bf
ivan.smialko <ivan.smialko@ab-games.com>
2025-05-13 23:22:28 +0300

    [Section 12][More multiplayer features] Game menu basic code for exiting match

--- Source/ShooterJam/Public/HUD/GameMenu.h
+++ Source/ShooterJam/Public/HUD/GameMenu.h
@@ -6,12 +6,33 @@
 #include "Blueprint/UserWidget.h"
 #include "GameMenu.generated.h"
 
-/**
- * 
- */
+class UButton;
+class UMultiplayerSessionsSubsystem;
+
 UCLASS()
 class SHOOTERJAM_API UGameMenu : public UUserWidget
 {
 	GENERATED_BODY()
 	
+private:
+	UPROPERTY(meta = (BindWidget))
+	UButton* ExitButton;
+
+	APlayerController* PlayerController{ nullptr };
+	UMultiplayerSessionsSubsystem* MultiplayerSessionsSubsystem{ nullptr };
+
+public:
+	void MenuSetup();
+	void MenuTeardown(); 
+
+protected:
+	virtual bool Initialize() override;
+
+	UFUNCTION()
+	void OnDestroySession(bool bWasSuccessfull);
+
+private:
+
+	UFUNCTION()
+	void OnExitButtonClicked();
 };
