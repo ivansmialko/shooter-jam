7c59d716ed1197187cd843e6e08b5ae35efe2022
Ivan Smialko <ivan.smialko@ab-games.com>
2025-05-30 23:57:48 +0300

    [Section 14][Teams] Team battle ui control class

---
+++ Source/ShooterJam/Public/HUD/TeamBattleWidget.h
@@ -0,0 +1,17 @@
+// Made by smialko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TeamBattleWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERJAM_API UTeamBattleWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
