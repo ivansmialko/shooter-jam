a4b30e1c310ed966430bcfa1dbb93260a32600f1
ivan.smialko <ivan.smialko@ab-games.com>
2025-06-17 20:11:33 +0300

    [GUI][Main menu] Working on new game menu. New commit message format

---
+++ Source/ShooterJam/Private/HUD/MainMenuWidget.cpp
@@ -0,0 +1,14 @@
+// Made by smialko
+
+
+#include "HUD/MainMenuWidget.h"
+
+void UMainMenuWidget::MenuSetup()
+{
+	InitializeButtonListeners();
+}
+
+void UMainMenuWidget::InitializeButtonListeners()
+{
+
+}
---
+++ Source/ShooterJam/Public/PlayerControllers/MainMenuPlayerController.h
@@ -0,0 +1,41 @@
+// Made by smialko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainMenuPlayerController.generated.h"
+
+class UMainMenuWidget;
+
+UCLASS()
+class SHOOTERJAM_API AMainMenuPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> MainMenuBlueprint;
+
+	UMainMenuWidget* MainMenuWidget;
+
+//public methods
+public:
+	virtual void OnPossess(APawn* InPawn) override;
+
+//protected methods
+protected:
+	void InitializeInput();
+	void CreateMenu();
+
+	UFUNCTION()
+	void OnClickedHost();
+	UFUNCTION()
+	void OnClickedJoin();
+	UFUNCTION()
+	void OnClickedTraining();
+	UFUNCTION()
+	void OnClickedSettings();
+	UFUNCTION()
+	void OnClickedExit();
+};
