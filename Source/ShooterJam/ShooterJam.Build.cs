fe96ba11cfc7315cb6632895363b3dfbd1334268
ivan.smialko <ivan.smialko@ab-games.com>
2025-08-05 21:47:59 +0300

    [Net][Sessions] Finally fixed sessions issue. Migrated to SteamSockets

--- Source/ShooterJam/ShooterJam.Build.cs
+++ Source/ShooterJam/ShooterJam.Build.cs
@@ -8,7 +8,7 @@ public class ShooterJam : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara", "MultiplayerSessions", "OnlineSubsystem", "OnlineSubsystemSteam" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara", "MultiplayerSessions", "OnlineSubsystem", "OnlineSubsystemSteam", "SteamSockets" });
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 	}
 }
